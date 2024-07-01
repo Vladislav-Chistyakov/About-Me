e58566d0eef0ff7991158edd97751a2c363b6d79
Vladislav_Chistyakov <vladischis@gmail.com>
2024-07-01 20:56:52 +0300

    Базовая настройка

---
+++ vite-project/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Main from "./components/Main/index.vue";
+</script>
+
+<template>
+  <div>
+    <Main />
+    <h1>123123</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite-project/src/components/Main/index.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "index",
+  data () {
+    return {
+      text: '11111'
+    }
+  },
+  computed: {
+    test () {
+      return 'test 111123123123123' + this.text
+    }
+  }
+}
+</script>
+
+<template>
+  <div>{{ test }}</div>
+</template>
+
+<style scoped>
+
+</style>
